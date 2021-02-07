1fcd109c0b94e4e13013f8968c892b1660f104e6
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 19:54:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
- monopoly =  {
+monopoly =  {
   :railroads=>
   {
     :pieces=>4,
@@ -62,26 +62,25 @@ def monopoly_with_fourth_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
-        "mortgage_value"=>"$100"
+        :mortgage_value => 100
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
-        "mortgage_value"=>"$200"
+        :mortgage_value => 200 
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
-        "mortgage_value"=>"$400"
+        :mortgage_value => 400 
       },
-      :shortline_railroad=>
       {
-        "mortgage_value"=>"$800"
+        :shortline =>
+      {
+        :mortgage_value => 800 
       }
-    }
   }
 }
-  
 end
